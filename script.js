f57f89b44a07779ec833248791d9e37665741fbc
aeternum-vale <aeternum-vale@ya.ru>
2017-01-21 15:50:45 +0300

    добавил взрывы после выстрелов

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ var SHOOT_BUTTON = 17;
 var GLOBAL_WIDTH = 2000;
 var GLOBAL_HEIGHT = 2000;
 
-
 var canvas = document.getElementsByTagName('canvas')[0];
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
@@ -23,12 +22,14 @@ var pressedButtons = {};
 var player = new Player();
 var map = new Map();
 
+var explosions = [];
+
 window.onkeydown = function (e) {
 	pressedButtons[e.keyCode] = true;
 };
 
 window.onkeyup = function (e) {
-	pressedButtons[e.keyCode] = false;
+	delete pressedButtons[e.keyCode];
 };
 
 setInterval(function () {
@@ -46,6 +47,7 @@ function clearScreen() {
 function drawFrame() {
 	ctx.save();
 
+	ctx.beginPath();
 	ctx.rect(0.5, 0.5, CANVAS_WIDTH - 1, CANVAS_HEIGHT - 1);
 	ctx.stroke();
 
@@ -57,11 +59,20 @@ function draw() {
 	clearScreen();
 	map.draw();
 	player.draw();
+
+	for (var i = 0; i < explosions.length; i++)
+		explosions[i].draw();
+
 	drawFrame();
 }
 
 function update() {
 	player.update();
+
+	for (var i = 0; i < explosions.length; i++)
+		if (explosions[i].update())
+			explosions.splice(i--, 1);
+
 	map.update();
 
 	/*
@@ -90,26 +101,27 @@ function Point(x, y) {
 		this.x += point.x;
 		this.y += point.y;
 	};
-	
+
 }
+
+
+
 
 function Player() {
 
 	var RADIUS = 10;
-	var LINE_WIDTH = 1;
-	var ALPHA = -70;
+	var DRAWING_LINE_WIDTH = 1;
+	var BARREL_WIDTH_ANGLE = -70;
 	var BARREL_LENGTH = 10;
 
 	var BASE_RADIUS = RADIUS * 1.3;
 
 
 	var ROTATION_SPEED = 8;
-
-	var RIDING_ENERGY_AMOUNT = 15;
-	var SHOOTING_ENERGY_AMOUNT = 30;
+	var SHOOTING_ENERGY_AMOUNT = 15;
 
 
-	var globalPosition = new Point(CANVAS_WIDTH/2, CANVAS_HEIGHT - RADIUS - LINE_WIDTH - BARREL_LENGTH);
+	var globalPosition = new Point(CANVAS_WIDTH/2, CANVAS_HEIGHT - RADIUS - DRAWING_LINE_WIDTH - BARREL_LENGTH);
 	var localPosition = new Point();
 
 	var p0 = new Point();
@@ -117,37 +129,19 @@ function Player() {
 
 	var angle = 0;
 
-
-
-
 	var aim = new Aim();
 
-	/*
-	this.setAngle = function(_angle) {
-		angle = _angle;
-	};
-	
-	this.getAngle = function() {
-		return angle;
-	};
-	*/
 
-
-	this.getGlobalPosition = function() {
-		return globalPosition;
+	this.getVisibleAreaGlobalPosition = function() {
+		return new Point(globalPosition.x - localPosition.x, globalPosition.y - localPosition.y);
 	};
-
-	this.getLocalPosition = function() {
-		return localPosition;
-	};
-
 
 	var impulses = [];
 	function Impulse (energyAmount, angle) {
 		var REDUCE_RATE = .1;
 		var energyStock = energyAmount;
 
-		this.reduceEnergyStock = function () {
+		this.update = function () {
 
 			energyStock -= REDUCE_RATE * energyStock;
 			return energyStock >= 1;
@@ -165,7 +159,7 @@ function Player() {
 	var shootButtonPressed = false;
 
 	var MAX_SPEED = 10;
-	var SPEED_INCREASE = 1.3;
+	var SPEED_INCREASE_RATE = 1.3;
 	var currentSpeed = 0;
 
 	
@@ -180,12 +174,30 @@ function Player() {
 		angle %= 360;
 
 
+		//shooting
+
+		if (pressedButtons[SHOOT_BUTTON] && !shootButtonPressed) {
+			shootButtonPressed = true;
+			impulses.push(new Impulse(SHOOTING_ENERGY_AMOUNT, angle + 90));
+			var aiml = aim.getLocalPosition();
+			var vagp = this.getVisibleAreaGlobalPosition();
+
+			explosions.push(new Explosion(new Point(aiml.x + vagp.x, aiml.y + vagp.y)));
+		}
+
+		if (!pressedButtons[SHOOT_BUTTON]) {
+			shootButtonPressed = false;
+		}
+
+
+		//moving
+
 
 		if (pressedButtons[UP_BUTTON] || pressedButtons[DOWN_BUTTON]) {
 			if (currentSpeed == 0)
 				currentSpeed = 1;
 			else
-				currentSpeed *= SPEED_INCREASE;
+				currentSpeed *= SPEED_INCREASE_RATE;
 
 			if (currentSpeed > MAX_SPEED)
 				currentSpeed = MAX_SPEED;
@@ -217,16 +229,7 @@ function Player() {
 			currentSpeed = 0;
 		}
 
-		//shooting
-
-		if (pressedButtons[SHOOT_BUTTON] && !shootButtonPressed) {
-			shootButtonPressed = true;
-			impulses.push(new Impulse(SHOOTING_ENERGY_AMOUNT, angle + 90));
-		}
-
-		if (!pressedButtons[SHOOT_BUTTON]) {
-			shootButtonPressed = false;
-		}
+		
 
 
 		//impulses
@@ -235,7 +238,7 @@ function Player() {
 		for (var i = 0; i < impulses.length; i++) {
 			globalPosition.offset(impulses[i].getOffsetVector());
 
-			if (!impulses[i].reduceEnergyStock())
+			if (!impulses[i].update())
 				impulses.splice(i--, 1);
 		}
 
@@ -265,10 +268,10 @@ function Player() {
 				localPosition.y = globalPosition.y - (GLOBAL_HEIGHT - CANVAS_HEIGHT);
 		}
 
-		p0.x = RADIUS * Math.cos(degToRad(ALPHA));
-		p0.y = RADIUS * Math.sin(degToRad(ALPHA));
-		p1.x = RADIUS * Math.cos(Math.PI - degToRad(ALPHA));
-		p1.y = RADIUS * Math.sin(Math.PI - degToRad(ALPHA));
+		p0.x = RADIUS * Math.cos(degToRad(BARREL_WIDTH_ANGLE));
+		p0.y = RADIUS * Math.sin(degToRad(BARREL_WIDTH_ANGLE));
+		p1.x = RADIUS * Math.cos(Math.PI - degToRad(BARREL_WIDTH_ANGLE));
+		p1.y = RADIUS * Math.sin(Math.PI - degToRad(BARREL_WIDTH_ANGLE));
 
 
 		aim.update();
@@ -282,7 +285,7 @@ function Player() {
 		ctx.translate(localPosition.x, localPosition.y);
 		ctx.rotate(degToRad(angle));
 
-		ctx.lineWidth = LINE_WIDTH;
+		ctx.lineWidth = DRAWING_LINE_WIDTH;
 		ctx.fillStyle = "#ccc";
 
 		ctx.beginPath();
@@ -294,7 +297,7 @@ function Player() {
 		ctx.beginPath();
 		ctx.moveTo(p0.x, p0.y - BARREL_LENGTH);
 		ctx.lineTo(p0.x, p0.y);
-		ctx.arc(0, 0, RADIUS, degToRad(ALPHA), Math.PI - degToRad(ALPHA) );
+		ctx.arc(0, 0, RADIUS, degToRad(BARREL_WIDTH_ANGLE), Math.PI - degToRad(BARREL_WIDTH_ANGLE) );
 		ctx.lineTo(p1.x, p1.y - BARREL_LENGTH);
 		ctx.closePath();
 
@@ -314,20 +317,25 @@ function Player() {
 		var AIM_RADIUS = 10;
 		var AIM_SMALL_RADIUS = AIM_RADIUS * .6;
 		var AIM_ROTATION_SPEED = 10;
+		var ALPHA = 15;
 
-		var coords = new Point();
+
+		var aimLocalPosition = new Point();
 		var aimAngle = 0;
+
+		this.getLocalPosition = function () {
+			return aimLocalPosition;
+		};
 
 		this.draw = function() {
 			ctx.save();
-			var alpha = 15;
-			var beta = 50;
+
 
 			ctx.lineWidth = 2;
 			ctx.fillStyle = "#ccc";
 			ctx.lineCap = "round";
 
-			ctx.translate(coords.x, coords.y);
+			ctx.translate(aimLocalPosition.x, aimLocalPosition.y);
 
 			ctx.beginPath();
 			ctx.arc(0, 0, AIM_SMALL_RADIUS, 0, Math.PI * 2);
@@ -338,7 +346,7 @@ function Player() {
 
 			for (var i = 0; i < 4; i++) {
 				ctx.beginPath();
-				ctx.arc(0, 0, AIM_RADIUS, degToRad(-alpha + 90 * i), degToRad(alpha + 90 * i));
+				ctx.arc(0, 0, AIM_RADIUS, degToRad(-ALPHA + 90 * i), degToRad(ALPHA + 90 * i));
 				ctx.stroke();
 			}
 
@@ -347,8 +355,8 @@ function Player() {
 
 
 		this.update = function() {
-			coords.x = localPosition.x + AIM_DIST * Math.cos(degToRad(angle - 90));
-			coords.y = localPosition.y + AIM_DIST * Math.sin(degToRad(angle - 90));
+			aimLocalPosition.x = localPosition.x + AIM_DIST * Math.cos(degToRad(angle - 90));
+			aimLocalPosition.y = localPosition.y + AIM_DIST * Math.sin(degToRad(angle - 90));
 
 
 			aimAngle -= AIM_ROTATION_SPEED;
@@ -357,6 +365,125 @@ function Player() {
 		};
 	}
 
+}
+
+
+function Explosion(globalPosition) {
+
+	var LIFETIME = 10;
+	var MAX_RADIUS = 100;
+
+	var currentFrame = 0;
+	var currentRadius;
+	var opacity = 1;
+
+	var localPosition = new Point();
+
+	var fbAmount = randomInteger(6, 10);
+	var firebolts = [];
+
+	for (var i = 0; i < fbAmount; i++)
+		firebolts.push(new Firebolt( Math.random() * 360 / fbAmount + i * 360 / fbAmount ));
+
+
+	this.update = function() {
+
+		var vagp = player.getVisibleAreaGlobalPosition();
+		localPosition.x =  globalPosition.x - vagp.x;
+		localPosition.y =  globalPosition.y - vagp.y;
+
+		currentRadius = MAX_RADIUS * currentFrame/LIFETIME;
+
+		if  (currentFrame/LIFETIME >= .7)
+			opacity = 1 - ((currentFrame/LIFETIME - .7) * 10/3);
+
+
+		for (var i = 0; i < firebolts.length; i++)
+			firebolts[i].update();
+
+
+		if (currentFrame++ >= LIFETIME)
+			return true;
+
+	};
+
+	this.draw = function() {
+		ctx.save();
+
+
+		ctx.fillStyle = "#F4FA58";
+		ctx.globalAlpha = opacity;
+
+		ctx.translate(localPosition.x, localPosition.y);
+
+		ctx.beginPath();
+		ctx.arc(0, 0, currentRadius, 0, Math.PI * 2);
+
+
+		//ctx.fill();
+
+		ctx.lineWidth = 15 * opacity;
+		ctx.strokeStyle = "yellow";
+		ctx.stroke();
+
+
+
+		ctx.restore();
+
+		for (var i = 0; i < firebolts.length; i++)
+			firebolts[i].draw();
+
+	};
+
+
+	function Firebolt(angle) {
+
+		var speed = Math.random() * 15 + 5; //15;
+
+		var fbLocalPosition = new Point();
+
+		var glowd = Math.random() * 10 + 20;//25;
+		var h1, h2;
+
+
+		var gradient;
+
+		this.update = function() {
+
+			fbLocalPosition.x = localPosition.x + currentFrame * speed * Math.cos(degToRad(angle));
+			fbLocalPosition.y = localPosition.y + currentFrame * speed * Math.sin(degToRad(angle));
+
+			h1 = new Point(fbLocalPosition.x + glowd * Math.cos(degToRad(angle + 90)),
+				fbLocalPosition.y + glowd * Math.sin(degToRad(angle + 90)));
+
+			h2 = new Point(fbLocalPosition.x + glowd * Math.cos(degToRad(angle - 90)),
+				fbLocalPosition.y + glowd * Math.sin(degToRad(angle - 90)));
+
+		};
+
+
+		this.draw = function() {
+			ctx.save();
+
+
+			ctx.globalAlpha = opacity;
+
+			gradient = ctx.createRadialGradient(fbLocalPosition.x, fbLocalPosition.y, 10,
+				fbLocalPosition.x, fbLocalPosition.y, currentFrame * speed);
+			gradient.addColorStop(0, "hsl(30,93%,60%)");
+			gradient.addColorStop(1, "hsl(63,91%,50%)");
+			ctx.fillStyle = gradient;
+
+			ctx.beginPath();
+			ctx.moveTo(localPosition.x, localPosition.y);
+			ctx.bezierCurveTo(h2.x, h2.y, fbLocalPosition.x, fbLocalPosition.y, fbLocalPosition.x, fbLocalPosition.y);
+			ctx.bezierCurveTo(h1.x, h1.y, localPosition.x, localPosition.y,  localPosition.x, localPosition.y);
+			ctx.fill();
+
+			ctx.restore();
+		};
+
+	}
 
 }
 
@@ -367,11 +494,10 @@ function Map() {
 
 
 	this.update = function () {
-		var g = player.getGlobalPosition();
-		var l = player.getLocalPosition();
+		var vagp = player.getVisibleAreaGlobalPosition();
 
-		startPoint.x = SEGMENT_SIZE - ((g.x - l.x) % SEGMENT_SIZE);
-		startPoint.y = SEGMENT_SIZE - ((g.y - l.y) % SEGMENT_SIZE);
+		startPoint.x = SEGMENT_SIZE - (vagp.x % SEGMENT_SIZE);
+		startPoint.y = SEGMENT_SIZE - (vagp.y % SEGMENT_SIZE);
 	};
 
 	this.draw = function () {
@@ -395,4 +521,10 @@ function Map() {
 		ctx.restore();
 	};
 
+}
+
+function randomInteger(min, max) {
+	var rand = min + Math.random() * (max - min)
+	rand = Math.round(rand);
+	return rand;
 }
